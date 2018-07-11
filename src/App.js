378d63dc07debad5f1bb703ced6951cc8f155b7e
Veselin Tonev <veselinn16@gmail.com>
2018-07-11 16:51:16 +0300

    Make first use of API

--- src/App.js
+++ src/App.js
@@ -4,19 +4,11 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookShelves from './BookShelves';
 import SearchPage from './SearchPage';
-import OpenSearch from './OpenSearch';
 
 
 class BooksApp extends Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
     books: [],
-    showSearchPage: false
   }
 
   componentDidMount() {
@@ -29,8 +21,7 @@ class BooksApp extends Component {
     return (
       <div className="app">
         <Route exact path='/' render={() => (
-          <BookShelves />
-          // <OpenSearch />
+          <BookShelves books={this.state.books} />
         )}/>
         
         <Route path='/search' render={() => (
--- src/BookShelves.js
+++ src/BookShelves.js
@@ -1,23 +1,63 @@
-import React from 'react';
-import CurrentlyReadingShelf from './CurrentlyReading'
+import React, { Component } from 'react';
+// import CurrentlyReadingShelf from './CurrentlyReading'
+import PropTypes from "prop-types";
 import WantToReadShelf from './WantToRead';
 import ReadShelf from './Read';
 import OpenSearch from './OpenSearch';
 
-function BookShelves() {
-    return (
+class BookShelves extends Component {
+    static propTypes = {
+        books: PropTypes.array.isRequired
+    }
+
+    render() {
+        const { books } = this.props
+
+        return (
         <div className="list-books">
             <div className="list-books-title">
                 <h1>MyReads</h1>
             </div>
+
             <div className="list-books-content">
-                <CurrentlyReadingShelf />
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">Currently Reading</h2>
+                    <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {books.map( book => 
+                            <li key={book.id}>
+                                <div className="book">
+                                    <div className="book-top">
+                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }} />
+                                    <div className="book-shelf-changer">
+                                        <select>
+                                        <option value="move" disabled>
+                                            Move to...
+                                        </option>
+                                        <option value="currentlyReading">
+                                            Currently Reading
+                                        </option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                        </select>
+                                    </div>
+                                    </div>
+                                    <div className="book-title">{book.title}</div>
+                                    <div className="book-authors">{book.authors}</div>
+                                </div>
+                            </li>
+                        )}
+                        </ol>
+                        </div>
+                </div>
                 <WantToReadShelf />
                 <ReadShelf />
             </div>
             <OpenSearch/>
         </div>
-    )
+        )
+    }
 }
 
 export default BookShelves
