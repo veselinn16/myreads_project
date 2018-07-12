d11436b865d40dd856ea6a9843bab21d7383c234
Veselin Tonev <veselinn16@gmail.com>
2018-07-12 15:55:42 +0300

    Use Book Component on Search page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import * as BooksAPI from "./BooksAPI";
+import Book from './Book'
 
 
 class SearchPage extends Component {
@@ -48,28 +49,7 @@ class SearchPage extends Component {
                 <ol className="books-grid">
                 {(this.state.newBooks) &&
                   (this.state.newBooks.map(book =>
-                    <li key={book.id}>
-                      <div className="book">
-                        <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }} />
-                          <div className="book-shelf-changer">
-                            <select>
-                              <option value="move" disabled>
-                                Move to...
-                                </option>
-                              <option value="currentlyReading">
-                                Currently Reading
-                                </option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">{book.authors}</div>
-                      </div>
-                    </li>
+                    <Book book={book}/>
                   ))}
                 </ol>
               </div>
