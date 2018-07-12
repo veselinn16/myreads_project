98195e1addf9b69ca321dca0847195c19d473d4b
Veselin Tonev <veselinn16@gmail.com>
2018-07-12 16:02:36 +0300

    Correctly set shelf state for Book Component

--- src/Book.js
+++ src/Book.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 
 class Book extends Component {
+  state = {
+    shelf: this.props.book.shelf
+  };
 
   render() {
-    const { book } = this.props
+    const { book } = this.props;
 
     return (
       <li key={book.id}>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }} />
+            <div
+              className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage: `url(${book.imageLinks.thumbnail})`
+              }}
+            />
             <div className="book-shelf-changer">
               <select id="shelf">
                 <option value="move" disabled>
                   Move to...
                 </option>
-                <option value="currentlyReading" >
-                  Currently Reading
-                </option>
+                <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
                 <option value="read">Read</option>
                 <option value="none">None</option>
@@ -28,7 +36,7 @@ class Book extends Component {
           <div className="book-authors">{book.authors}</div>
         </div>
       </li>
-    )
+    );
   }
 }
 
