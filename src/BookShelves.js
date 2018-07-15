b845e175012c3710d71b86302ec0cb07cdbcb7f0
Veselin Tonev <veselinn16@gmail.com>
2018-07-15 10:34:23 +0300

    Refactor class component into stateless functional one

--- src/BookShelves.js
+++ src/BookShelves.js
@@ -3,62 +3,56 @@ import PropTypes from 'prop-types'
 import OpenSearch from './OpenSearch'
 import Book from './Book'
 
-class BookShelves extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired
-  }
+const BookShelves = (props) => {
+  const { books } = props
 
-  render() {
-    const { books } = this.props
+  return <div className="list-books">
+      <div className="list-books-title">
+        <h1 className="title">MyReads - a Book Trading App</h1>
+      </div>
 
-    return <div className="list-books">
-        <div className="list-books-title">
-          <h1 className="title">MyReads - a Book Trading App</h1>
+      <div className="list-books-content">
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Currently Reading</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {books.map(book => book.shelf ===  'currentlyReading' && 
+              <li key={book.id}>
+                <Book book={book} shelf={book.shelf} changeShelf={props.changeShelf}/>
+              </li>
+              )}
+            </ol>
+          </div>
         </div>
 
-        <div className="list-books-content">
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {books.map(book => book.shelf ===  'currentlyReading' && 
-                <li key={book.id}>
-                  <Book book={book} shelf={book.shelf} changeShelf={this.props.changeShelf}/>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Want To Read</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {books.map(book => book.shelf ===    'wantToRead' &&
+                <li key={book.id}> 
+                  <Book book={book} shelf={book.shelf} changeShelf={props.changeShelf}/>
                 </li>
-                )}
-              </ol>
-            </div>
+              )}
+            </ol>
           </div>
+        </div>
 
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Want To Read</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {books.map(book => book.shelf ===    'wantToRead' &&
-                  <li key={book.id}> 
-                    <Book book={book} shelf={book.shelf} changeShelf={this.props.changeShelf}/>
-                  </li>
-                )}
-              </ol>
-            </div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Read</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {books.map(book => book.shelf === "read" && 
+              <li key={book.id}>
+                <Book book={book} shelf={book.shelf} changeShelf={props.changeShelf}/>
+              </li>
+              )}
+            </ol>
           </div>
-
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Read</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {books.map(book => book.shelf === "read" && 
-                <li key={book.id}>
-                  <Book book={book} shelf={book.shelf} changeShelf={this.props.changeShelf}/>
-                </li>
-                )}
-              </ol>
-            </div>
-          </div>
-        <OpenSearch />
-      </div>
+        </div>
+      <OpenSearch />
     </div>
-  }
+  </div>
 }
 
 export default BookShelves
